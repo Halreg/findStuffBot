c0d56b89db0c0c42c1e982832db0ac860cbc7e43
NikkkkaaBabych <magictime.jkrowling@gmail.com>
2021-05-15 02:07:39 +0300

    additional functionality

--- src/main/java/com/example/service/dbrelatedservices/PostQueries.java
+++ src/main/java/com/example/service/dbrelatedservices/PostQueries.java
@@ -2,10 +2,12 @@ package com.example.service.dbrelatedservices;
 
 import com.example.model.Post;
 import com.example.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@Slf4j
 public class PostQueries {
 
     @Autowired
@@ -16,6 +18,7 @@ public class PostQueries {
     }
 
     public static void SavePost(final Post post) {
+        log.info(post.toString());
         postRepository.save(post);
     }
 
