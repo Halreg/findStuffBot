aa2c1ae1660585c8a5cf974a7aad165a32e5b253
NikkkkaaBabych <magictime.jkrowling@gmail.com>
2021-05-15 02:00:09 +0300

    additional functionality

--- src/main/java/com/example/repository/PostRepository.java
+++ src/main/java/com/example/repository/PostRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends ElasticsearchRepository<Post, String> {
 
 
--- src/main/java/com/example/service/dbrelatedservices/PostQueries.java
+++ src/main/java/com/example/service/dbrelatedservices/PostQueries.java
@@ -2,11 +2,13 @@ package com.example.service.dbrelatedservices;
 
 import com.example.model.Post;
 import com.example.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class PostQueries {
 
+    @Autowired
     private static PostRepository postRepository;
 
     public static void createProductIndexBulk(final List<Post> posts) {
