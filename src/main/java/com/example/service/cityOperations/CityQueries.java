efebf8ecb003fb0e2f0caec34bf55de0f8c59954
NikkkkaaBabych <magictime.jkrowling@gmail.com>
2021-05-29 19:09:22 +0300

    additional functionality

--- src/main/java/com/example/service/cityOperations/CityQueries.java
+++ src/main/java/com/example/service/cityOperations/CityQueries.java
@@ -29,7 +29,7 @@ public class CityQueries {
     public List<City> searchCity(String name){
         NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
                     .withQuery(org.elasticsearch.index.query.QueryBuilders
-                            .matchQuery("name", name)).build();
+                            .matchQuery("city", name)).build();
         SearchHits<City> sampleEntities =
                     elasticsearchTemplate.search(searchQuery,City.class, IndexCoordinates.of("cities"));
         List<SearchHit<City>> searchHits = sampleEntities.getSearchHits();
