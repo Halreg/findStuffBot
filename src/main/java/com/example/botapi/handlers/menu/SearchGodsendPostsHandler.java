d11358e076390709f1d00a9b012abf927e9951ad
NikkkkaaBabych <magictime.jkrowling@gmail.com>
2021-06-05 08:17:20 +0300

    additional functionality

--- src/main/java/com/example/botapi/handlers/menu/SearchGodsendPostsHandler.java
+++ src/main/java/com/example/botapi/handlers/menu/SearchGodsendPostsHandler.java
@@ -2,6 +2,10 @@ package com.example.botapi.handlers.menu;
 
 import com.example.botapi.BotState;
 import com.example.botapi.handlers.InputMessageHandler;
+import com.example.cache.UserDataCache;
+import com.example.service.postsearching.PostSearchCache;
+import com.example.service.postsearching.PostSearchState;
+import com.example.service.postsearching.SearchPostsService;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -9,9 +13,18 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 @Component
 public class SearchGodsendPostsHandler implements InputMessageHandler {
 
+    private final SearchPostsService searchPostsService;
+    private final UserDataCache userDataCache;
+
+    private SearchGodsendPostsHandler(SearchPostsService searchPostsService,UserDataCache userDataCache){
+        this.searchPostsService = searchPostsService;
+        this.userDataCache = userDataCache;
+    }
+
     @Override
     public SendMessage handle(Message message) {
-        return new SendMessage(message.getChatId(), "Search Godsend");
+        PostSearchCache postSearchCache = userDataCache.getSearchPostsCache(message.getFrom().getId(), PostSearchState.GODSEND);
+        return searchPostsService.getRepliedText(message,postSearchCache,userDataCache,message.getFrom().getId());
     }
 
     @Override
--- src/main/java/com/example/botapi/handlers/menu/SearchLostPostsHandler.java
+++ src/main/java/com/example/botapi/handlers/menu/SearchLostPostsHandler.java
@@ -2,6 +2,10 @@ package com.example.botapi.handlers.menu;
 
 import com.example.botapi.BotState;
 import com.example.botapi.handlers.InputMessageHandler;
+import com.example.cache.UserDataCache;
+import com.example.service.postsearching.PostSearchCache;
+import com.example.service.postsearching.PostSearchState;
+import com.example.service.postsearching.SearchPostsService;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -9,9 +13,18 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 @Component
 public class SearchLostPostsHandler implements InputMessageHandler {
 
+    private final SearchPostsService searchPostsService;
+    private final UserDataCache userDataCache;
+
+    private SearchLostPostsHandler(SearchPostsService searchPostsService,UserDataCache userDataCache){
+        this.searchPostsService = searchPostsService;
+        this.userDataCache = userDataCache;
+    }
+
     @Override
     public SendMessage handle(Message message) {
-        return new SendMessage(message.getChatId(), "Search Lost");
+        PostSearchCache postSearchCache = userDataCache.getSearchPostsCache(message.getFrom().getId(), PostSearchState.LOSS);
+        return searchPostsService.getRepliedText(message,postSearchCache,userDataCache,message.getFrom().getId());
     }
 
     @Override
